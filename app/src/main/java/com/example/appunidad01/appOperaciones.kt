be2f25fc92ed691b9c554df71bca94b44df6db13
estefania-hm <estefaniahm165@gmail.com>
2026-05-29 13:55:44 -0700

    3e declaró los objetos en la clase appOperaciones

--- app/src/main/java/com/example/appunidad01/appOperaciones.kt
+++ app/src/main/java/com/example/appunidad01/appOperaciones.kt
@@ -7,14 +7,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class appOperaciones : AppCompatActivity() {
+    private lateinit var txtNum1 : EditText
+    private lateinit var txtNum2 : EditText
+    private lateinit var spnOperaciones: Spinner
+    private lateinit var txtResultado: TextView
+    private lateinit var imgOperacion: ImageView
+    private lateinit var btnCalcular : Button
+    private lateinit var btnLimpiar : Button
+    private lateinit var btnCerrar : Button
+    private var pos : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_app_operaciones)
+        iniciarComponentes()
+        eventoClic()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-}
