01da12b23512ee5de21106add1b0f5a629c2942f
estefania-hm <estefaniahm165@gmail.com>
2026-06-09 21:27:53 -0700

    Se codifico el mainActivity

--- app/src/main/java/com/example/appunidad01/calculoNomina.kt
+++ app/src/main/java/com/example/appunidad01/calculoNomina.kt
@@ -1,20 +1,34 @@
 package com.example.appunidad01
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class calculoNomina : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_calculo_nomina)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val txtNomExamen2 = findViewById<EditText>(R.id.txtNomExamen2)
+        val btnEntrarExamen2 = findViewById<Button>(R.id.btnEntrarExamen2)
+        val btnSalirExamen2 = findViewById<Button>(R.id.btnSalirExamen2)
+
+        btnEntrarExamen2.setOnClickListener {
+            val nombre = txtNomExamen2.text.toString().trim()
+            if (nombre.isEmpty()) {
+                Toast.makeText(this, "Todos los datos son requeridos", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, ReciboNominaActivity::class.java)
+                intent.putExtra("NOMBRE_TRABAJADOR", nombre)
+                startActivity(intent)
+            }
+        }
+
+        btnSalirExamen2.setOnClickListener {
+            finish()
         }
     }
 }
