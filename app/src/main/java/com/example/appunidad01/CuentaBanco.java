1e4f1d1f1ad54396d8aa5416e3147f5574f6ce44
estefania-hm <estefaniahm165@gmail.com>
2026-06-15 12:39:04 -0700

    Se realizo la clase Cuenta Banco

---
+++ app/src/main/java/com/example/appunidad01/CuentaBanco.java
@@ -0,0 +1,23 @@
+package com.example.appunidad01;
+class CuentaBanco(
+        var numCuenta: String,
+        var nombre: String,
+        var banco: String,
+        var saldo: Float
+) {
+    fun obtenerSaldo(): Float {
+        return saldo
+    }
+
+    fun retirarDinero(cantidad: Float): Boolean {
+        if (cantidad <= saldo) {
+            saldo -= cantidad
+            return true
+        }
+        return false
+    }
+
+    fun hacerDeposito(cantidad: Float) {
+        saldo += cantidad
+    }
+}
