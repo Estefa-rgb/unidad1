e34c1645cc84227961ddf6ec5bda8f132b549586
estefania-hm <estefaniahm165@gmail.com>
2026-06-15 12:41:49 -0700

    Se realizo la codificación del MainActivity

--- app/src/main/java/com/example/appunidad01/appBanco.kt
+++ app/src/main/java/com/example/appunidad01/appBanco.kt
@@ -12,14 +12,14 @@ class appBanco : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_app_banco)
 
         val txtUser = findViewById<EditText>(R.id.txtUser)
         val txtPassword = findViewById<EditText>(R.id.txtPassword)
         val btnIngresar = findViewById<Button>(R.id.btnIngresar)
 
-        findViewById<TextView>(R.id.tvEmpresa).text = getString(R.string.banco)
-        findViewById<TextView>(R.id.tvDomicilio).text = getString(R.string.domcilio)
+        findViewById<TextView>(R.id.tvEmpresa).text = getString(R.string.nombre_banco)
+        findViewById<TextView>(R.id.tvDomicilio).text = getString(R.string.domicilio_banco)
 
         btnIngresar.setOnClickListener {
             val user = txtUser.text.toString()
@@ -30,7 +30,7 @@ class appBanco : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            if (user == getString(R.string.user) && pass == getString(R.string.pass)) {
+            if (user == getString(R.string.user_banco) && pass == getString(R.string.pass_banco)) {
                 val intent = Intent(this, CuentBancoActivity::class.java)
                 intent.putExtra("usuario", user)
                 startActivity(intent)
